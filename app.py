efb266c14e497ab9bd001fcd67e10ae337a589fe
TheOwner <theowner@toaster.thehome.com>
2026-02-21 11:31:53 +0900

    feat: Add variables that will be sent to FFmpeg command processor class.

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ class App(tk.Tk):
     self.geometry('800x400')
     self.resizable(False, False)
 
+    """ FFmpeg options to compress video """
+    self.input_file = ""
+    self.format = "mp4"
+    self.resolution = "1920x1080"
+    self.codec = "libx264"
+    self.fps = "30"
+    self.quality = 90
+    self.remove_audio = False
+
 if __name__ == "__main__":
   clip_optimization_app = App()
   clip_optimization_app.mainloop()
