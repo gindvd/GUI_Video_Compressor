140d29c8f58480fc361ec38c74e64bb7fe16702b
TheOwner <theowner@toaster.thehome.com>
2026-02-26 21:41:38 +0900

    feat: Add function to return video's duration.

--- ffprobe_processor.py
+++ ffprobe_processor.py
@@ -4,3 +4,21 @@ class FFprobeProcessor():
   def __init__(self, ffprobe):
     self.ffprobe = ffprobe
     
+  def get_duration(self, filepath):
+    command = [self.ffprobe, "-v", "error", "-show_entries", 
+               "format=duration", "-of", 
+               "default=noprint_wrappers=1:nokey=1", 
+               "-sexagesimal", filepath]
+    
+    try:
+      duration, _ = subprocess.Popen(command, stdout=subprocess.PIPE, 
+                                stderr=subprocess.PIPE, shell=False, text=True)
+    
+      return duration
+    
+    except FileNotFoundError as err:
+      pass
+    except subprocess.CalledProcessError as err:
+      pass
+    except Exception as err:
+      pass  
