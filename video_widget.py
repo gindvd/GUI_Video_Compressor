fc0b2452347b8cb62ddd17ed68cfc013a54b206f
TheOwner <theowner@toaster.thehome.com>
2026-02-28 10:58:36 +0900

    feat: Add function to load video and get video info.

--- video_widget.py
+++ video_widget.py
@@ -5,10 +5,17 @@ class VideoPlayer(ctk.CTkFrame):
   def __init__(self):
     super().__init__()
 
-    self["bg"] = 'black'
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme("blue")
     
     self.video_file = ""
-    self.video_player = TkV(master=self, scaled=True, keep_aspect=True, 
+    self.duration = 0
+    self.start_time = -1
+    self.end_time = 1
+    self.currrent_time = 0
+    self.new_duration = 0
+    
+    self.video_player = TkinterVideo(self, scaled=True, keep_aspect=True, 
                             consistent_frame_rate=True,  bg="black")
     
     self.video_player.set_resampling_method(1)
@@ -17,4 +24,20 @@ class VideoPlayer(ctk.CTkFrame):
     self.video_player.bind("<<SecondChanged>>", self.update_scale)
     self.video_player.bind("<<Ended>>", self.video_ended)
     
-    self.progress_slider = ctk.CTkSlider(master=self, from_=-1, to=1, number+of+steps=1, command=self.seek)
+    self.play_pause_btn = ctk.CTkButton(self, text="Play", command=self.play_pause)
+    self.play_pause_btn.pack(padx=10, pady=10)
+    
+  def load_video(self, filename):
+    self.video_file = filename 
+    self.video_player.stop()
+    
+    try:
+      self.video_player.load(self.video_file)
+      
+      self.duration = int(self.video_player.video_info()["duration"])
+      self.end_time = int(self.video_player.video_info()["duration"])
+      
+         
+    except:
+      CTkMessagebox(title="Loading Failure", message="ERROR!\nMVideo file couldn't be opened!", 
+                    icon="error")
