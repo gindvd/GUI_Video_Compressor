82d5dcb8dd483ec813d5f0b8239580c0e4ef44c9
TheOwner <theowner@toaster.thehome.com>
2026-03-01 20:53:53 +0900

    fix: Change how window is closed.

--- ctk_progressbar_popup.py
+++ ctk_progressbar_popup.py
@@ -5,7 +5,6 @@ class ProgressbarPopup(ctk.CTkToplevel):
     super().__init__(parent)
 
     self.title("Progress Bar")
-    self.resizable(False, False)
     
     ctk.set_appearance_mode("System")  
     ctk.set_default_color_theme("blue")
@@ -16,8 +15,8 @@ class ProgressbarPopup(ctk.CTkToplevel):
 
     self.progressbar.pack(padx=10, pady=10)
 
-  def start(self):
+  def start_bar(self):
     self.progressbar.start()
 
-  def destroy(self):
-    self.quit()
+  def destroy_window(self):
+    self.destroy()
