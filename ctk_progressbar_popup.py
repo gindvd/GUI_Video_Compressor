2ec020acb524fb1f1ee8f5cc3887dcd9a81fc04f
TheOwner <theowner@toaster.thehome.com>
2026-03-02 22:52:35 +0900

    fix: Change init.

--- ctk_progressbar_popup.py
+++ ctk_progressbar_popup.py
@@ -5,17 +5,21 @@ class ProgressbarPopup(ctk.CTkToplevel):
     super().__init__(parent)
 
     self.title("Progress Bar")
+    self.resizable(False, False)
+
+    self.lift()
+    self.attributes("-topmost", True)
     
     ctk.set_appearance_mode("System")  
     ctk.set_default_color_theme("blue")
 
-    self.progressbar = ctk.CTkProgressBar(self, 
+    self.progressbar = ctk.CTkProgressBar(self,
                                           orientation='horizontal', 
-                                          mode='indeterminate')
+                                          mode='indeterminate',
+                                          determinate_speed=0.75)
 
-    self.progressbar.pack(padx=10, pady=10)
-
-  def start_bar(self):
+    self.progressbar.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+  
     self.progressbar.start()
 
   def destroy_window(self):
