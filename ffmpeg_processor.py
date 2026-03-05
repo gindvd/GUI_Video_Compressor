0f9be41820a1140d3dcb0f9cbc8d3def05fc0895
gindvd <gingerichdvd@gmail.com>
2026-03-05 20:12:10 +0900

    fix: Change class method and variable names to follow Python naming sstandards.

--- ffmpeg_processor.py
+++ ffmpeg_processor.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
+import platform
 
 class FFmpegProcessor():
   def __init__(self, ffmpeg):
-    self.ffmpeg = ffmpeg
+    self._ffmpeg = ffmpeg
+    self._proc = None
 
   def compress(self, input_file, file_format, resolution, codec, fps, quality, audio):
     basename, _ = os.path.splitext(input_file)
@@ -12,30 +14,30 @@ class FFmpegProcessor():
     width, height = resolution.split("x")
     scale = "scale={}:{}".format(width, height)
     
-    crf = self.crf_converter(quality)
+    crf = self._crf_converter(quality)
     
     if codec == "libvpx-vp9":
       audio_codec = "libopus"
     else:
       audio_codec = "aac"
 
-    command = [self.ffmpeg,
-               "-i", input_file, 
-               "-c:v", codec,
-               "-r", fps,
-               "-crf", str(crf), 
-               "-vf", scale]
+    cmd = [self._ffmpeg,
+           "-i", input_file, 
+           "-c:v", codec,
+           "-r", fps,
+           "-crf", str(crf), 
+           "-vf", scale]
 
     if not audio:
-      command.extend(["-an"])
+      cmd.extend(["-an"])
     elif audio:
-        command.extend(["-c:a", audio_codec, 
-                        "-b:a", "128k"]) 
+        cmd.extend(["-c:a", audio_codec, 
+                    "-b:a", "128k"]) 
     
     command.extend([output_file])
 
     try:
-      proc = subprocess.Popen(command,
+      proc = subprocess.Popen(cmd,
                               stdout=subprocess.PIPE, 
                               stderr=subprocess.PIPE,
                               shell=False,
