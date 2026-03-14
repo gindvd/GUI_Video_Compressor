10087c6a4830a2d541e595e243bf24ff66c63444
TheOwner <theowner@toaster.thehome.com>
2026-03-14 23:16:36 +0900

    feat: Add better layout.

--- src/components/progressbar_popup.py
+++ src/components/progressbar_popup.py
@@ -5,7 +5,7 @@ class ProgressbarPopup(ctk.CTkToplevel):
     super().__init__(parent)
     self._parent = parent
 
-    self.title("Progress Bar")
+    self.title("Compression in Progress")
     self.resizable(False, False)
 
     self.lift()
@@ -13,18 +13,26 @@ class ProgressbarPopup(ctk.CTkToplevel):
     ctk.set_appearance_mode("System")  
     ctk.set_default_color_theme("blue")
 
-    self._progressbar = ctk.CTkProgressBar(self,
+    self._btm_frame = ctk.CTkFrame(self, corner_radius=0)
+
+    self._progressbar = ctk.CTkProgressBar(self._btm_frame,
+                                           height=15,
+                                           width=300,
                                            orientation='horizontal', 
                                            mode='indeterminate',
                                            determinate_speed=0.75)
 
-    self._progressbar.grid(row=0, column=0, columnspan=3, padx=50, pady=50)
+    self._progressbar.pack(padx=30, pady=50)
+
+    self._btm_frame.pack(expand=True, fill='both')
     
     self._cancel_compression_btn = ctk.CTkButton(self, 
                                                  text="Cancel", 
                                                  command=self._parent.cancel_compression)  
 
-    self._cancel_compression_btn.grid(row=2,column=2, padx=10, pady=10)
+    self._cancel_compression_btn.pack(side="right", padx=10, pady=10)
+
+    
   
   def run_progressbar(self):
     self._progressbar.start()
