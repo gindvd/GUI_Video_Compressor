5c3293d471db421fab36f63e805d219ffe76464f
TheOwner <theowner@toaster.thehome.com>
2026-03-15 19:44:10 +0900

    feat: Change vlc instance to use executable in special folder for Windows.

--- src/components/video_trimmer.py
+++ src/components/video_trimmer.py
@@ -4,5 +4,34 @@ import vlc
 import platform
 
 class VideoTrimmer(CTkFrame):
-    def __init__(self):
-        super().__init__()
+  def __init__(self):
+    super().__init__()
+
+    self._instance = self._platform_specific_inst()
+
+  def _platform_specific_inst(self):
+
+    if platform.system() == "Windows":
+
+      try:
+        vlc_path = os.path.abspath("lib/win32/vlc-win32.exe")
+
+        except FileNotFoundError:
+          close = CTkMessagebox(title="Missing FFmpeg Exe", 
+                                message="""
+                                        ERROR!\n
+                                        vlc-win32.exe is missing from lib folder!\n
+                                        Please ensure FFmpeg is installed correctly!
+                                        """, 
+                                icon="cancel",
+                                option_1="Ok")
+        
+          if close.get() == "Ok":
+            self.quit()
+        
+        else:
+            return vlc.Instance("--plugin-path={}".format(vlc_path))
+    
+    else:
+        return vlc.Instance()
+      
