357dd9d968f11968d4332f65f31146636a7e58d3
TheOwner <theowner@toaster.thehome.com>
2026-03-21 18:33:49 +0900

    feat: Change utils to use pathlib library.

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,15 @@
-import os
+from pathlib import Path
 from datetime import datetime
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = Path(__file__).parent[1]
 
 def create_logs(err_msg):
   now = datetime.now()
+  basename = str(now) + ".log"
 
-  log_basename = str(now) + ".log"
-
-  log_dir_path = os.path.join(ROOT_DIR, 'logs')
-  os.makedirs(log_dir_path, exist_ok=True)
-
-  log_file = os.path.join(log_dir_path, log_basename)
+  log_directory = ROOT_DIR / 'logs'
+  log_directory.mkdir(exist_ok=True)
+  log_file = log_directory / basename
 
   with open(log_file, 'w') as f:
     f.write(err_msg)
