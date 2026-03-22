cc2fff73dc18206c15f45f8b5e8f7c2ffb5be6d2
TheOwner <theowner@toaster.thehome.com>
2026-03-22 11:22:18 +0900

    fix: Add missing commas

--- src/modules/GPUinfo.py
+++ src/modules/GPUinfo.py
@@ -17,7 +17,7 @@ class OSCompatibiltyError(Exception):
 
 CMD_DICT = {
   "Linux" : "lspci | grep -iE VGA|3D|video",
-  "Darwin" : "system_profiler SPDisplaysDataType"
+  "Darwin" : "system_profiler SPDisplaysDataType",
   "Windows" : {
     "11" : "powershell -Command Get-CimInstance Win32_VideoController | Select-Object name",
     "legacy" : "wmic path win32_VideoController get name"
