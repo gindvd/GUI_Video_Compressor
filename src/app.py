a90646d2ac2577fba10080f14fd75d54eaa5ab0c
gindvd <gingerichdvd@gmail.com>
2026-03-22 23:36:59 +0900

    fix: Typo

--- src/app.py
+++ src/app.py
@@ -333,7 +333,7 @@ class App(ctk.CTk):
         case "Intel":
           # Older itel GPUs on Linux have qsv support depreciated and or lacking :(
           if DEVICE_OS == "Linux":
-            codecs.extend(["h264_vaapi", "hevc_vaap`i"])
+            codecs.extend(["h264_vaapi", "hevc_vaapi"])
           else:
             codecs.extend(["h264_qsv", "hevc_qsv"])
         case _:
