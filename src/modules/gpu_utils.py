da2771e65c40b8edb84cddcdfc39f4322fe0cde2
gindvd <gingerichdvd@gmail.com>
2026-04-13 20:44:18 +0900

    fix: Change how Powershell command is run due to it failing

--- src/modules/gpu_utils.py
+++ src/modules/gpu_utils.py
@@ -15,68 +15,62 @@ class OSCompatibiltyError(Exception):
     return f"{self._message} (Non-Compatible OS: {self._os})\nCompatible OS: Windows , Linux"
 
 CMD_DICT: dict = {
-  "Linux" : "lspci | grep -iE VGA|3D|video",
-  "Darwin" : "system_profiler SPDisplaysDataType",
+  "Linux" : {
+    "parent" : ["lspci"],
+    "child" : ["grep", "-iE", "VGA|3D|video"],
+  },
+  "Darwin" : {
+    "parent" : ["system_profiler", "SPDisplaysDataType"],
+  },
   "Windows" : {
-    "11" : "powershell -Command Get-CimInstance Win32_VideoController | Select-Object name",
-    "legacy" : "wmic path win32_VideoController get name"
-    }
+    "11" : {
+      "parent" : ["powershell", "-Command",
+                   "Get-CimInstance Win32_VideoController | Select-Object -ExpandProperty Name"],
+    },
+    "legacy" : {
+      "parent" : ["wmic", "path", "Win32_VideoController", "get", "name"],
+    },
+  },
 }
 
 def get_card_info() -> list[str] | None:
-  try:
-    if DEVICE_OS not in ["Windows", "Linux", "Darwin"]:
-      raise OSCompatibiltyError("Current OS is not compatible with this module.", DEVICE_OS)
-  
-  except OSCompatibiltyError as e:
-    create_logs(str(e))
+  if DEVICE_OS not in ["Windows", "Linux", "Darwin"]:
+    create_logs(str(OSCompatibiltyError("Current OS is not compatible with this module.", DEVICE_OS)))
     return None
 
   if DEVICE_OS == "Windows":
     win_ver = platform.release()
-  
     if win_ver != "11":
       win_ver = "legacy"
-      
-    cmd =  CMD_DICT.get(DEVICE_OS, {}).get(win_ver)
-  
-  else:
-    cmd = CMD_DICT.get(DEVICE_OS)
     
-  # Need to seperate commands in 2 if it contains a pipe
-  # Then turn both commands into lists
-  cmd_lists: list[list[str]] = [word.split() for word in cmd.split('|', 1)]
-  
-  assert len(cmd_lists) <= 2, "Command list contains too many lists of commands, Max Num of list: 2"
+    cmd_entry = CMD_DICT.get(DEVICE_OS, {}).get(win_ver)
+  else:
+    cmd_entry = CMD_DICT.get(DEVICE_OS)
 
-  parent_cmd: list[str] = cmd_lists[0]
-  child_cmd: list[str] | None = None
-
-  try:
-    if parent_cmd is None:
-      raise AttributeError
-
-  except AttributeError:
-    create_logs("Parent command set to None")
-
+  if cmd_entry is None:
+    create_logs(f"No command configuration found for OS: {DEVICE_OS}")
     return None
-  
-  if len(cmd_lists) == 2:
-    child_cmd = cmd_lists[1]
 
-  if child_cmd == None:
+  parent_cmd: list[str] = cmd_entry.get("parent")
+  child_cmd: list[str] | None = cmd_entry.get("child")
+
+  if parent_cmd is None:
+    create_logs("Parent command set to None")
+    return None
+
+  if child_cmd is None:
     return run_cmd(parent_cmd)
 
   return run_piped_cmd(parent_cmd, child_cmd)
 
 def run_cmd(cmd: list[str]) -> list[str] | None:
-  proc = subprocess.Popen(cmd, 
-                          stdout=subprocess.PIPE, 
-                          stderr=subprocess.PIPE, 
-                          shell=False, 
-                          text=True)
-
   try:
+    proc = subprocess.Popen(cmd, 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.PIPE, 
+                            shell=False, 
+                            text=True)
+
     out, err = proc.communicate()
     proc.wait()
     
@@ -84,59 +78,53 @@ def run_cmd(cmd: list[str]) -> list[str] | None:
 
   except FileNotFoundError as e:
     create_logs(str(e))
-
     return None
   
   except Exception as e:
     create_logs(str(e))
-
     return None
 
   else:
     if rc != 0:
       create_logs(err)
-      
       return None
 
     return out.split()
 
 
 def run_piped_cmd(cmd1: list[str], cmd2: list[str]) -> list[str] | None:
-  proc1 = subprocess.Popen(cmd1,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE,
-                           shell=False,
-                           text=True)
-
-  proc2 = subprocess.Popen(cmd2,
-                           stdin=proc1.stdout,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE,
-                           shell=False,
-                           text=True) 
-  
   try:
-    assert proc1.stdout is not None
-    proc1.stdout.close()
-    out, err = proc2.communicate()
+    proc1 = subprocess.Popen(cmd1,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=False,
+                             text=True)
 
+    proc2 = subprocess.Popen(cmd2,
+                             stdin=proc1.stdout,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=False,
+                             text=True) 
+
+    if proc1.stdout is not None:
+      proc1.stdout.close()
+    
+    out, err = proc2.communicate()
     proc2.wait()
     rc = proc2.returncode
 
   except FileNotFoundError as e:
     create_logs(str(e))
-
     return None
   
   except Exception as e:
     create_logs(str(e))
-
     return None
     
   else:
     if rc != 0:
       create_logs(err)
-
       return None
 
     return out.split()
