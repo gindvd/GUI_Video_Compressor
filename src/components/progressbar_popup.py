f280719309afe1b689e71f33bfc068e2be0d95b0
gindvd <gingerichdvd@gmail.com>
2026-04-17 07:40:43 +0900

    fix: Add functions to center progress bar pop up in the center of master's window

--- src/components/progressbar_popup.py
+++ src/components/progressbar_popup.py
@@ -1,14 +1,12 @@
 import customtkinter as ctk
 
 class ProgressbarPopup(ctk.CTkToplevel):
-  def __init__(self, parent) -> None:
-    super().__init__(parent)
-    self._parent = parent
+  def __init__(self, master) -> None:
+    super().__init__(master)
+    self._master = master
 
     self.title("Compression in Progress")
     self.resizable(False, False)
-
-    self.lift()
     
     ctk.set_appearance_mode("System")  
     ctk.set_default_color_theme("blue")
@@ -28,11 +26,29 @@ class ProgressbarPopup(ctk.CTkToplevel):
     
     self._cancel_compression_btn = ctk.CTkButton(self, 
                                                  text="Cancel", 
-                                                 command=self._parent.cancel_compression)  
+                                                 command=self._master.cancel_compression)  
 
     self._cancel_compression_btn.pack(side="right", padx=10, pady=10)
 
-    self.protocol("WM_DELETE_WINDOW", self._parent.cancel_compression)
+    self.update_idletasks()
+    master_x = self._master.winfo_x()
+    master_y = self._master.winfo_y()
+    master_w = self._master.winfo_width()
+    master_h = self._master.winfo_height()
+    popup_w = self.winfo_width()
+    popup_h = self.winfo_height()
+    x = master_x + (master_w - popup_w) // 2
+    y = master_y + (master_h - popup_h) // 2
+    self.geometry(f"+{x}+{y}")
+
+    self.lift(self._master)
+    self.attributes("-topmost", True)
+    self.after(200, lambda: self.attributes("-topmost", False))
+    self.grab_set()
+    self.focus()
+
+    # Cancels compression if progress bar is closed
+    self.protocol("WM_DELETE_WINDOW", self._master.cancel_compression)
   
   def run_progressbar(self) -> None:
     self._progressbar.start()
