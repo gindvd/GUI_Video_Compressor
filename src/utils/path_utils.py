ab9e583aa5961601720f490f28b854ab80a53f86
gindvd <gingerichdvd@gmail.com>
2026-05-12 20:21:03 +0900

    fix: Change how app gets resources from lib and assets folder

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -9,13 +9,13 @@ def resource_path(relative_path: str = "") -> Path:
   if getattr(sys, 'frozen', False):
     base_path = Path(sys._MEIPASS)
   else:
-    base_path = Path(__file__).parents[2]
+    base_path = Path(__file__).parents[1]
   return base_path / relative_path
 
 def get_app_dir() -> Path:
   if getattr(sys, 'frozen', False):
     return Path(sys.executable).parent
-  return Path(__file__).parents[2]
+  return Path(__file__).parents[1]
 
 def setup_vlc_environment() -> None:
   if system() != "Windows":
